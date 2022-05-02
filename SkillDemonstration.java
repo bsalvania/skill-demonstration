5e9cdfef0b5813bbdd69c048301324d10f05a546
bsalvania <bsalvania@ucsd.edu>
2022-05-01 17:51:56 -0700

    Added method and a tester file

--- SkillDemonstration.java
+++ SkillDemonstration.java
@@ -1,5 +1,7 @@
 public class SkillDemonstration {
 
-
+    public static int multiplication(int num1, int num2) {
+        return num1 * num2;
+    }
 
 }
---
+++ SkillDemonstrationTester.java
@@ -0,0 +1,10 @@
+import static org.junit.Assert.*;
+import org.junit.*;
+
+public class SkillDemonstrationTester {
+    
+    @Test 
+    public void multiplicationTester() {
+        assertEquals(10, SkillDemonstration.multiplication(2, 4));
+    }
+}
